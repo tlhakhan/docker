33862a371aaba91f95fc636a4e311063df888ac2
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2020-06-27 15:34:28 -0400

    create a build all script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# find all build scripts and execute them
+#
+
+# find all os builds first
+find ./os -type f -name build.sh -execdir ./{} ';'
+
+# find all app builds
+find ./app -type f -name build.sh -execdir ./{} ';'
