7fa4f1ddcdfbee26422cbd15748a8c5d80b31fbc
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2020-10-18 19:31:01 +0000

    updated dockerfile

--- app/smokeping/build.sh
+++ app/smokeping/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker build . \
-  -t smokeping-app:latest \
-  -t repo.home.local/tlhakhan/app/smokeping-app:latest
-  -t repo.home.local/tlhakhan/app/smokeping-app:$(date +%Y%m%d)
+  -t smokeping:latest \
+  -t repo.home.local/tlhakhan/app/smokeping:latest \
+  -t repo.home.local/tlhakhan/app/smokeping:$(date +%Y%m%d)
 
-docker push repo.home.local/tlhakhan/app/smokeping-app:latest
-docker push repo.home.local/tlhakhan/app/smokeping-app:$(date +%Y%m%d)
+docker push repo.home.local/tlhakhan/app/smokeping:latest
+docker push repo.home.local/tlhakhan/app/smokeping:$(date +%Y%m%d)
