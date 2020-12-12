69b86c7c6add205fc38db44c1e2634ea0eae1b67
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2020-12-12 03:43:37 +0000

    updated squid

--- app/squid/build.sh
+++ app/squid/build.sh
@@ -5,5 +5,5 @@ docker build . \
   -t repo.home.local/tlhakhan/app/squid:latest \
   -t repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
 
-docker push repo.home.local/tlhakhan/app/squid:latest
-docker push repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
+#docker push repo.home.local/tlhakhan/app/squid:latest
+#docker push repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
