277980b7cf5993a890b9f44b9854b288a72322bf
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-01-23 15:39:46 +0000

    updated ansible container

--- app/ansible/build.sh
+++ app/ansible/build.sh
@@ -3,6 +3,8 @@
 readlink -f . | xargs -n1 basename | figlet
 sleep 1
 
-docker build . -t ansible:latest -t repo.home.local/tlhakhan/app/ansible:latest
+docker build . \
+  -t ansible:latest \
+  -t repo.home.local/tlhakhan/app/ansible:latest
 
 docker push repo.home.local/tlhakhan/app/ansible:latest
