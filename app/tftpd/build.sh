fcd0726ec3bf5b81c675c88c024d5b9bb19b21c8
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-01-24 17:32:03 +0000

    fix build script

--- app/tftpd/build.sh
+++ app/tftpd/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker build . -t tftpd:latest -t repo.home.local/tlhakhan/app/tftpd:latest
+
+readlink -f . | xargs -n1 basename | figlet
+sleep 1
+
+docker build . \
+  -t tftpd:latest \
+  -t repo.home.local/tlhakhan/app/tftpd:latest
 
 docker push repo.home.local/tlhakhan/app/tftpd:latest
