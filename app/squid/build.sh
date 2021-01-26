68bdea8c78b0b011fedd300d59eed847542b3be8
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2021-01-26 02:20:39 +0000

    easy squid app

--- app/squid/build.sh
+++ app/squid/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+readlink -f . | xargs -n1 basename | figlet
+sleep 1
+
 docker build . \
   -t squid:latest \
   -t repo.home.local/tlhakhan/app/squid:latest \
   -t repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
 
-#docker push repo.home.local/tlhakhan/app/squid:latest
-#docker push repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
+docker push repo.home.local/tlhakhan/app/squid:latest
+docker push repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
