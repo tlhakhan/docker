6fdb377e62183ef51ad1e068705fa009730b6adf
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-06-17 17:06:52 +0000

    do not need nginx image

--- app/nginx/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-readlink -f . | xargs -n1 basename | figlet
-sleep 1
-
-docker build . -t nginx:latest -t repo.home.local/tlhakhan/app/nginx:latest
-
-docker push repo.home.local/tlhakhan/app/nginx:latest
