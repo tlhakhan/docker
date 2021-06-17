eee542dcd84ad2711d052cc8ac572c5e88312c2f
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-06-17 15:48:55 +0000

    updated crond app image

--- app/crond/build.sh
+++ app/crond/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e
+
+REPO_SERVER=repo.tenzin.io
 
 readlink -f . | xargs -n1 basename | figlet
 sleep 1
 
 docker build . \
   -t crond:latest \
-  -t repo.home.local/tlhakhan/app/crond:latest
+  -t ${REPO_SERVER}/tlhakhan/app/crond:latest
 
-docker push repo.home.local/tlhakhan/app/crond:latest
+docker login ${REPO_SERVER}
+docker push ${REPO_SERVER}/tlhakhan/app/crond:latest
