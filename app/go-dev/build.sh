aa6b6900d486e689ebb108361141f24133d89233
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-06-17 17:07:31 +0000

    os native packages are now up-to-date

--- app/go-dev/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-readlink -f . | xargs -n1 basename | figlet
-sleep 1
-
-docker build . -t go-dev:latest -t repo.home.local/tlhakhan/app/go-dev:latest
-docker push repo.home.local/tlhakhan/app/go-dev:latest
