7e1b5e5efdcd33d98669192ba0069fcf67570879
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-06-17 17:13:22 +0000

    working squid image

--- app/squid/build.sh
+++ app/squid/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+set -e
+
+REPO_SERVER=repo.tenzin.io
+
 readlink -f . | xargs -n1 basename | figlet
 sleep 1
 
 docker build . \
   -t squid:latest \
-  -t repo.home.local/tlhakhan/app/squid:latest \
-  -t repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
+  -t ${REPO_SERVER}/tlhakhan/app/squid:latest \
+  -t ${REPO_SERVER}/tlhakhan/app/squid:$(date +%Y%m%d)
 
-docker push repo.home.local/tlhakhan/app/squid:latest
-docker push repo.home.local/tlhakhan/app/squid:$(date +%Y%m%d)
+docker login ${REPO_SERVER}
+
+docker push ${REPO_SERVER}/tlhakhan/app/squid:latest
+docker push ${REPO_SERVER}/tlhakhan/app/squid:$(date +%Y%m%d)
