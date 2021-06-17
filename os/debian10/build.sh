73958c36f3abef481e8bd1620b52ab784cc5ea26
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-06-17 15:42:42 +0000

    updated debian10 for new repo

--- os/debian10/build.sh
+++ os/debian10/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
+REPO_SERVER=repo.tenzin.io
+
 readlink -f . | xargs -n1 basename | figlet
 sleep 1
 
 docker build . \
   -t debian:10 \
-  -t repo.home.local/tlhakhan/os/debian:10 \
-  -t repo.home.local/tlhakhan/os/debian:10-$(date +%Y%m%d)
+  -t ${REPO_SERVER}/tlhakhan/os/debian:10 \
+  -t ${REPO_SERVER}/tlhakhan/os/debian:10-$(date +%Y%m%d)
 
-docker push \
-  repo.home.local/tlhakhan/os/debian:10
-docker push \
-  repo.home.local/tlhakhan/os/debian:10-$(date +%Y%m%d)
+docker login ${REPO_SERVER}
+
+docker push ${REPO_SERVER}/tlhakhan/os/debian:10
+docker push ${REPO_SERVER}/tlhakhan/os/debian:10-$(date +%Y%m%d)
