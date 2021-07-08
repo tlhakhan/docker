1dd5e76080c09962a29d4b9b0d8a99ea4f49d668
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2021-07-08 17:04:06 +0000

    update build script

--- app/opengrok/build.sh
+++ app/opengrok/build.sh
@@ -2,15 +2,16 @@
 set -e
 
 REPO_SERVER=repo.tenzin.io
+IMAGE_TAG=1.7
 
 readlink -f . | xargs -n1 basename | figlet
 sleep 1
 
 docker build . \
   -t opengrok:latest \
-  -t ${REPO_SERVER}/tlhakhan/app/opengrok:1.7.10 \
-  -t ${REPO_SERVER}/tlhakhan/app/opengrok:1.7.10-$(date +%Y%m%d)
+  -t ${REPO_SERVER}/tlhakhan/app/opengrok:${IMAGE_TAG}\
+  -t ${REPO_SERVER}/tlhakhan/app/opengrok:${IMAGE_TAG}-$(date +%Y%m%d)
 
 docker login ${REPO_SERVER}
-docker push ${REPO_SERVER}/tlhakhan/app/opengrok:1.7.10
-docker push ${REPO_SERVER}/tlhakhan/app/opengrok:1.7.10-$(date +%Y%m%d)
+docker push ${REPO_SERVER}/tlhakhan/app/opengrok:${IMAGE_TAG}
+docker push ${REPO_SERVER}/tlhakhan/app/opengrok:${IMAGE_TAG}-$(date +%Y%m%d)
